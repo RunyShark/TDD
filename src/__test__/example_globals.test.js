ea609427e025fbfef3dc0f9bffc53aa821aeb457
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-05 22:26:40 -0500

    first commit

---
+++ src/__test__/example_globals.test.js
@@ -0,0 +1,13 @@
+describe('this is for group it case', () => {
+  it('description', () => {
+    expect(true).toBe(true);
+  });
+
+  it('description 2', () => {
+    expect(true).toBe(true);
+  });
+
+  it('description 3', () => {
+    expect(true).toBe(true);
+  });
+});
