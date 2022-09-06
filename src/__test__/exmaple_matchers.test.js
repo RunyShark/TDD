74ecf60e0cc52bd1b942a027a563c423d940dd8a
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-05 22:37:45 -0500

    exmaple_matchers.test.js

--- src/__test__/exmaple_matchers.test.js
+++ src/__test__/exmaple_matchers.test.js
@@ -1,1 +1,24 @@
 //*machers
+
+describe('matcher', () => {
+  it('toBe', () => {
+    expect(true).toBe(true);
+  });
+
+  it('toEqueal', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+
+    const array = ['one', 'two'];
+    expect(array).toEqual(['one', 'two']);
+  });
+
+  it('not', () => {
+    expect(true).not.toBe(false);
+  });
+
+  //   it('string', () => {
+  //     expect('team').toMatch('i');
+  //   });
+});
